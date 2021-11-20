d095dd169331b9918c2e0cd214710da75b3a98f4
yixun <yixun2010@outlook.com>
2021-11-20 12:24:14 +0800

    build data model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    topic = models.CharField(max_length=100)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'BlogPosts'
+
+    def __str__(self):
+        return self.topic[:10] + '...'
